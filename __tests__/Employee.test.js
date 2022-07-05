df5e49cab92db5e9c5774f5f9d06bbb8fe137b3b
hellovolcano <valerie.gleason@gmail.com>
2022-07-05 14:55:24 -0500

    adding employee properties and methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,33 @@ test('creates a new employee object', () => {
     expect(employee.id).toBe(1)
     expect(employee.email).toBe('hellovolcano@gmail.com')
 })
+
+test("get's an employee's name", () => {
+    const employee = new Employee('Valerie', 1, 'hellovolcano@gmail.com')
+    const name = employee.getName()
+
+    expect(name).toBe(employee.name)
+})
+
+test("gets an employee's ID", () => {
+    const employee = new Employee('Valerie', 1, 'hellovolcano@gmail.com')
+    const id = employee.getId()
+
+    expect(id).toBe(employee.id)
+
+})
+
+test("get's an employee's email address", () => {
+    const employee = new Employee('Valerie', 1, 'hellovolcano@gmail.com')
+    const email = employee.getEmail()
+
+    expect(email).toBe(employee.email)
+})
+
+test("get's an employee's role", () => {
+    const employee = new Employee('Valerie', 1, 'hellovolcano@gmail.com')
+    const role = employee.getRole()
+
+    expect(role).toBe('Employee')
+    
+})
