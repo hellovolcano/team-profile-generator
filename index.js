c5d3f27f28e75783c1bcc7bd5f2a99e65c52e609
hellovolcano <valerie.gleason@gmail.com>
2022-07-09 13:42:49 -0500

    fixing inquirer chaining issue so promise doesn't resolve before dataset is acquired

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer =require('inquirer')
 const Choices = require('inquirer/lib/objects/choices.js')
 const Manager = require('./lib/Manager.js')
+const generatePage = require('./src/page-template')
 
 const getManagerInfo = () => {
 
@@ -29,79 +30,71 @@ const getManagerInfo = () => {
     ])
 }
 
-const addMemberMenu = teamData => {
+const getEmployeeInfo = (teamData) => {
 
-    return inquirer.prompt([
-        {
-            type: 'list',
-            message: "What type of employee would you like to add next?",
-            name: 'choice',
-            choices: ['Engineer', 'Intern', 'Finalize the team']
-        }
-    ])
-    // Call the getEmployeeInfo function or exit, depending on user selection
-    .then(({ choice }) => {
-        if (choice === 'Finalize the team') {
-            console.log(teamData)
-            return teamData
-        }
-        // pass in the choice so we know what type of employee they're adding
-        getEmployeeInfo(teamData, choice)
-    })
-}    
+    return inquirer
+        .prompt({
+                type: 'list',
+                message: "What type of employee would you like to add next?",
+                name: 'choice',
+                choices: ['Engineer', 'Intern', 'Finalize the team']
+        })
+        .then(({ choice }) => {
+            // return the data and exit the function if the user selects the finalize option
+            if (choice === 'Finalize the team') {
+                return teamData
+            }
 
-const getEmployeeInfo = (teamData, choice) => {
-    // create an array to store the employee info if it doesn't exist yet
-    if(!teamData.employees) {
-        teamData.employees = []
-    }
+            // create an array to store the employee info if it doesn't exist yet
+            if(!teamData.employees) {
+                teamData.employees = []
+            }
     
-    inquirer
-      .prompt([
-    {
-        type: 'input',
-        message: 'Enter the name of the employee:',
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: 'Enter the employee ID',
-        name: 'id'
-    },
-    {
-        type: 'input',
-        message: 'Enter the email address for the employee:',
-        name: 'email'
-    },
-    {
-        type: 'input',
-        message: 'Enter the university this intern attends:',
-        name: 'school',
-        when: choice === 'Intern'
-    },
-    {
-        type: 'input',
-        message: 'Enter the github user name for the engineer:',
-        name: 'github',
-        when: choice === 'Engineer'
-    }
- ])
- // add the employee type to the object and then push to the employees array
- .then(employeeData => {
-    employeeData.type = choice
-    teamData.employees.push(employeeData)
-    // console.log(teamData)
-    addMemberMenu(teamData)
- })
+            return inquirer
+                .prompt([
+                {
+                    type: 'input',
+                    message: 'Enter the name of the employee:',
+                    name: 'name',
+                },
+                {
+                    type: 'input',
+                    message: 'Enter the employee ID',
+                    name: 'id'
+                },
+                {
+                    type: 'input',
+                    message: 'Enter the email address for the employee:',
+                    name: 'email'
+                },
+                {
+                    type: 'input',
+                    message: 'Enter the university this intern attends:',
+                    name: 'school',
+                    when: choice === 'Intern'
+                },
+                {
+                    type: 'input',
+                    message: 'Enter the github user name for the engineer:',
+                    name: 'github',
+                    when: choice === 'Engineer'
+                }
+            ])
+            // add the employee type to the object and then push to the employees array
+            .then(employeeData => {
+                employeeData.type = choice
+                teamData.employees.push(employeeData)
 
+                // Call the function again to display the manager action menu
+                return getEmployeeInfo(teamData)
+            })
+        })
 
 }
 
 getManagerInfo()
-    .then(addMemberMenu)
-    .then(
-        teamData => {
-        console.log(`this is in the then statement at the end: `)
-        console.log(teamData)
+    .then(getEmployeeInfo)
+    .then(teamData => {
+        return generatePage(teamData)
         }
 )
---
+++ src/page-template.js
@@ -0,0 +1,4 @@
+module.exports = teamData => {
+    console.log('Calling this from inside the generatePage function')
+    console.log(teamData)
+}
